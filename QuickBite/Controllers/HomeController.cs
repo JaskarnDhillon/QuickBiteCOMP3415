94bbb1ac71491b48b66d7022524ceba63cdfbf9e
Benjamin Noje <beninoje@hotmail.com>
2024-10-16 10:15:58 -0400

    UI done for auth

--- QuickBite/Controllers/HomeController.cs
+++ QuickBite/Controllers/HomeController.cs
@@ -15,13 +15,31 @@ namespace QuickBite.Controllers
 
         public IActionResult Index()
         {
+            /*ExampleModel exampleModel1 = new ExampleModel();
+            ExampleModel exampleModel2 = new ExampleModel();
+            exampleModel1.title = "Example Title 1";
+            exampleModel1.desc = "Example Desc 1";
+            
+            exampleModel2.title = "Example Title 2";
+            exampleModel2.desc = "Example Desc 2";
+            
+            IEnumerable<ExampleModel> exampleModels = new List<ExampleModel>{exampleModel1, exampleModel2 };*/
+            /*return View(exampleModels)*/
             return View();
+            
+        }
+
+        public class ExampleModel()
+        {
+            public string title { get; set; }
+            public string desc { get; set; }
         }
         [Route("/about")]
         public IActionResult About()
         {
             return View();
         }
+        
 
         public IActionResult Privacy()
         {
---
+++ QuickBite/Views/Shared/_ExamplePartial.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QuickBite.Views.Shared;
+
+public class _ExamplePartial : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
