0ad670785aedd83a34ea6d455b4ce23047a321bd
JaskarnDhillon <mail.jaskarndhillon@gmail.com>
2024-11-14 16:24:57 -0500

    fix

--- QuickBite/Controllers/RestaurantRegistrationController.cs
+++ QuickBite/Controllers/RestaurantRegistrationController.cs
@@ -1,10 +1,23 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using QuickBite.Data;
 using QuickBite.Models;
 
 namespace QuickBite.Controllers;
 
 public class RestaurantRegistrationController : Controller
 {
+    private readonly ApplicationDbContext _context;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    
+    public RestaurantRegistrationController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, IHttpContextAccessor httpContextAccessor)
+    {
+        _context = context;
+        _userManager = userManager;
+        _httpContextAccessor = httpContextAccessor;
+    }
+    
     // GET
     public IActionResult Index()
     {
@@ -13,8 +26,30 @@ public class RestaurantRegistrationController : Controller
     
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Index(Restaurant restaurant)
+    public async Task<IActionResult> Register([Bind("Name, Description, Photo, Address, Latitude, Longitude, DeliveryRadius, OpeningHour, CloseingHour")] Restaurant restaurant)
     {
+        ApplicationUser applicationUser = new();
+        try
+        {
+            var user = await _userManager.GetUserAsync(_httpContextAccessor.HttpContext.User);
+            if (user != null)
+            {
+                applicationUser = user;
+            }
+        }
+        catch (Exception ex)
+        {
+            string message = ex.Message;
+        }
+
+        restaurant.RestaurantOwner = applicationUser;
+        restaurant.RestaurantOwenrId = applicationUser.Id;
+        restaurant.isAccepted = false;
+        
+        _context.Restaurant.Add(restaurant);
+        await _context.SaveChangesAsync();
+        
+        
         return RedirectToAction(nameof(Index));
     }
 }
