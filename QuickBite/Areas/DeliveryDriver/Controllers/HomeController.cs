d0bec0197509f752024479f657344e34f02935e4
miran <miranararat722@gmail.com>
2024-11-24 18:58:24 -0500

    Delivery Driver can now mark order as delivered

--- QuickBite/Areas/DeliveryDriver/Controllers/HomeController.cs
+++ QuickBite/Areas/DeliveryDriver/Controllers/HomeController.cs
@@ -39,8 +39,57 @@ namespace QuickBite.Areas.DeliveryDriver.Controllers
 
             var restaurnt = _context.Restaurant.Include(r=>r.Orders).Where(r => r.RestaurantId==user).FirstOrDefault();
 
-            var orders = restaurnt.Orders.Where(o=>o.OrderStatus==OrderStatus.Ready).ToList();
+            var orders = restaurnt.Orders.Where(o=>o.OrderStatus==OrderStatus.Ready || o.OrderStatus==OrderStatus.OutForDelivery || o.OrderStatus==OrderStatus.Delivered).ToList();
             return View(orders);
         }
+
+        public IActionResult Details(Guid id)
+        {
+            var driverRestaurantId = _userManager.GetUserAsync(User).Result.RestaurantDeliveryDriverId;
+
+            if (driverRestaurantId == null)
+            {
+                return Unauthorized();
+            }
+
+            var order = _context.Orders
+                .Include(o => o.Restaurant)
+                .FirstOrDefault(o => o.OrderId == id && o.Restaurant.RestaurantId == driverRestaurantId);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult UpdateOrderStatus(Guid id, OrderStatus status)
+        {
+            var driverRestaurantId = _userManager.GetUserAsync(User).Result.RestaurantDeliveryDriverId;
+
+            if (driverRestaurantId == null)
+            {
+                return Unauthorized();
+            }
+
+            var order = _context.Orders
+                .Include(o => o.Restaurant)
+                .FirstOrDefault(o => o.OrderId == id && o.Restaurant.RestaurantId == driverRestaurantId);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            // Update the order status
+            order.OrderStatus = status;
+            _context.SaveChanges();
+
+            return RedirectToAction("AssignedOrders");
+        }
+
     }
 }
